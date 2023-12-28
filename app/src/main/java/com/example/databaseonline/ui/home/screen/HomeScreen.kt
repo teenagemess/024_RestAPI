4c2cbd3493e95b212809b89b8461518ca0669668
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 19:18:04 +0700

    membuat variable scroll Behavior

--- app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.example.databaseonline.ui.home.screen
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.databaseonline.navigation.DestinasiNavigasi
 import com.example.databaseonline.ui.home.viewmodel.HomeViewModel
@@ -10,8 +13,13 @@ object DestinasiHome : DestinasiNavigasi {
     override val titleRes = "Kontak"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
     onDetailClick: (Int) -> Unit = {},
-){}
+
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
