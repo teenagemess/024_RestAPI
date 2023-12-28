3940fbd41948b11bd61a5a6c45e4fed94651a735
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 19:12:00 +0700

    membuat object pada halaman HomeScreen.kt

---
+++ app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.databaseonline.ui.home.screen
+
+import com.example.databaseonline.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Kontak"
+}
