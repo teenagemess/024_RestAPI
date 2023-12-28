9874628142aebe6efe9c3c69404bf8ede22f96fd
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 18:59:46 +0700

    menambah komponen IconButton

--- app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -116,10 +118,9 @@ fun KontakCard(
                     style = MaterialTheme.typography.titleLarge,
                 )
                 Spacer(Modifier.weight(1f))
-                Icon(
-                    imageVector = Icons.Default.Phone,
-                    contentDescription = null,
-                )
+                IconButton(onClick = { onDeleteClick(kontak) }) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+                }
                 Text(
                     text = kontak.nohp,
                     style = MaterialTheme.typography.titleMedium
