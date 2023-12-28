50a441583dc27aa6e7e93e6e23b3db5f39f9cf93
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 18:48:09 +0700

    Membuat suspend fun deletekontak dengan tipe data boolean untuk confirm delete

--- app/src/main/java/com/example/databaseonline/repositori/KontakRepositori.kt
+++ app/src/main/java/com/example/databaseonline/repositori/KontakRepositori.kt
@@ -4,7 +4,7 @@ import com.example.databaseonline.modeldata.Kontak
 import com.example.databaseonline.service_api.KontakService
 
 interface KontakRepository {
-
+    suspend fun deleteKontak(id: Int): Boolean
     suspend fun getKontak(): List<Kontak>
 }
 
@@ -12,5 +12,12 @@ class NetworkKontakRepository(
     private val kontakApiService: KontakService
 ) : KontakRepository {
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+    override suspend fun deleteKontak(id: Int): Boolean {
+        // Implementasi penghapusan kontak melalui kontakApiService dengan menggunakan id
+        val response = kontakApiService.deleteKontak(id)
+
+        // Mengembalikan nilai true jika penghapusan berhasil, false sebaliknya
+        return response.isSuccessful
+    }
 }
 
