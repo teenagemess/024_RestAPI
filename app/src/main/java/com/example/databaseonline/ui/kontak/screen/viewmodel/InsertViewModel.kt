d472a28432652bb668f85b78688c35c3dd46918f
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 19:30:24 +0700

    membuat function InsertUiEvent

--- app/src/main/java/com/example/databaseonline/ui/kontak/screen/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/databaseonline/ui/kontak/screen/viewmodel/InsertViewModel.kt
@@ -1,9 +1,18 @@
 package com.example.databaseonline.ui.kontak.screen.viewmodel
 
+import com.example.databaseonline.modeldata.Kontak
+
 
 data class InsertUiEvent(
     val id: Int = 0,
     val nama: String = "",
     val email: String = "",
     val nohp: String = "",
+)
+
+fun InsertUiEvent.toKontak(): Kontak = Kontak(
+    id = id,
+    nama = nama,
+    email = email,
+    nohp = nohp
 )
