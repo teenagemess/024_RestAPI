a3fcfd52a2e2820c8d9687230b35c2e69775843f
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 19:40:15 +0700

    membuat class InsertViewModel untuk memanggil semua function yang ada di file tersebut

--- app/src/main/java/com/example/databaseonline/ui/kontak/screen/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/databaseonline/ui/kontak/screen/viewmodel/InsertViewModel.kt
@@ -1,8 +1,33 @@
 package com.example.databaseonline.ui.kontak.screen.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.databaseonline.modeldata.Kontak
+import com.example.databaseonline.repositori.KontakRepository
+import kotlinx.coroutines.launch
 
+class InsertViewModel (private val kontakRepository: KontakRepository) : ViewModel() {
 
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 
 data class InsertUiState(
