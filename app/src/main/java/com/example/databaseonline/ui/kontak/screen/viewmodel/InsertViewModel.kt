d68a8187e82f34e56deabd1043ad26b97af61eb0
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 19:34:28 +0700

    menambah function untuk memanggil InsertUiEvent

--- app/src/main/java/com/example/databaseonline/ui/kontak/screen/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/databaseonline/ui/kontak/screen/viewmodel/InsertViewModel.kt
@@ -3,15 +3,15 @@ package com.example.databaseonline.ui.kontak.screen.viewmodel
 import com.example.databaseonline.modeldata.Kontak
 
 
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
+
 data class InsertUiEvent(
     val id: Int = 0,
     val nama: String = "",
     val email: String = "",
     val nohp: String = "",
-)
-
-data class InsertUiState(
-    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
 )
 
 fun InsertUiEvent.toKontak(): Kontak = Kontak(
@@ -21,13 +21,13 @@ fun InsertUiEvent.toKontak(): Kontak = Kontak(
     nohp = nohp
 )
 
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent(),
+)
+
 fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
     id = id,
     nama = nama,
     email = email,
     nohp = nohp
-)
-
-fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
-    insertUiEvent = toInsertUiEvent(),
 )
