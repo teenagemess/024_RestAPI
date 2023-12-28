ed850182cf6412f4e16104bc49285d3667ef74a6
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 19:21:56 +0700

    menambah scaffold dan menambahkan komponen komponen didalamnya dan isi dari function HomeScreen akan memanggil HomeStatus

--- app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
@@ -1,10 +1,22 @@
 package com.example.databaseonline.ui.home.screen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.databaseonline.navigation.DestinasiNavigasi
+import com.example.databaseonline.ui.PenyediaViewModel
+import com.example.databaseonline.ui.TopAppBarKontak
 import com.example.databaseonline.ui.home.viewmodel.HomeViewModel
 
 
@@ -19,10 +31,35 @@ fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
     onDetailClick: (Int) -> Unit = {},
-
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(title = DestinasiHome.titleRes, canNavigateBack = false, scrollBehavior = scrollBehavior)
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            }
+        }
+    ){ innerPadding ->
+        HomeStatus(
+            kontakUIState = viewModel.kontakUIState,
+            retryAction = { viewModel.getKontak() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = {
+                viewModel.deleteKontak(it.id)
+                viewModel.getKontak()
+            }
+        )
+    }
 
 
 }
