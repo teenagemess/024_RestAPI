f1c26b985f90d988c98c842b85ea70f4b5506d6b
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 18:19:50 +0700

    membuat komponen CenterAlignedTopAppBar beserta parameter nya

--- app/src/main/java/com/example/databaseonline/ui/KontakApp.kt
+++ app/src/main/java/com/example/databaseonline/ui/KontakApp.kt
@@ -2,8 +2,12 @@ package com.example.databaseonline.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -41,7 +45,6 @@ fun KontakApp(
                 retryAction = homeViewModel::getKontak
             )
 
-
         }
     }
 }
@@ -54,4 +57,22 @@ fun TopAppBarKontak(
     title: String,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit = {}
-){}
+){
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        modifier = modifier,
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+                }
+            }
+        }
+    )
+}
