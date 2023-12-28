1a17dfe7e17967efd6e6617a624bbe5071485a11
teenagemess <raynanda.aqiyas.ft21@mail.umy.ac.id>
2023-12-28 19:15:45 +0700

    membuat function HomeScreen dan menambahkan paramaeter yang akan dibutuhkan nantinya

--- app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/databaseonline/ui/home/screen/HomeScreen.kt
@@ -1,8 +1,17 @@
 package com.example.databaseonline.ui.home.screen
 
+import androidx.compose.ui.Modifier
 import com.example.databaseonline.navigation.DestinasiNavigasi
+import com.example.databaseonline.ui.home.viewmodel.HomeViewModel
+
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Kontak"
 }
+
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+){}
